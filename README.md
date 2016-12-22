bfa9e55c2b47883e71a0fd6ab2944840f1ae9582
yangjunhua <yangjunhua@u51.com>
2016-12-22 10:20:21 +0800

    add project file

---
+++ README.md
@@ -0,0 +1,27 @@
+##### Installation
+    npm install flexbox.css --save
+
+##### Api
+    require('flexbox.css');
+
+
+##### CSS Class
+    .flex-row-box               // 主轴为x轴，弹性元素垂直居中
+    .flex-row-between-box       // 主轴为x轴，弹性元素两端对齐，适合做app头部，左端(返回)，中部(标题)，右端(链接)
+    .flex-column-box            // 主轴为y轴，弹性元素垂直居中
+    .flex-column-between-box    // 主轴为y轴，弹性元素两端对齐
+    .flex-center-box            // 弹性元素水平垂直居中
+    .flex-item-scroll-x         // 弹性元素flex:1，且设置了overflow-x: auto;
+    .flex-item-scroll-y         // 弹性元素flex:1，且设置了overflow-y: auto;
+
+    .flex-cell-box              // 删格布局，支持10列，flex-wrap:wrap;
+    .flex-cell-box .col-1       // 弹性元素width:100%;
+    .flex-cell-box .col-2       // 弹性元素width:50%; 以此类推
+    .flex-cell-box .col-3       // ...
+    .flex-cell-box .col-4
+    .flex-cell-box .col-5
+    .flex-cell-box .col-6
+    .flex-cell-box .col-7
+    .flex-cell-box .col-8
+    .flex-cell-box .col-9
+    .flex-cell-box .col-10
---
+++ compile/production.js
@@ -0,0 +1,37 @@
+let path = require('path');
+let Autoprefixer = require('autoprefixer');
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+// let HtmlWebpackPlugin = require('html-webpack-plugin');
+
+let loaders = [
+    { test: /\.js$/,   exclude: /node_modules/, loader: "babel" },
+    { test: /\.scss$/, loader: ExtractTextPlugin.extract("style", "css!postcss!sass") },
+    // { test: /\.html$/, loader: "html" }
+];
+
+let plugins = [
+    // new HtmlWebpackPlugin({
+    //     template: path.resolve(__dirname, '../src/index.html'),
+    //     filename: './index.html',
+    //     inject: true,
+    //     hash: true
+    // }),
+    new ExtractTextPlugin("index.css")
+];
+
+module.exports = {
+    entry: {
+        index: path.join(__dirname, '../src/index.js')
+    },
+    module: {
+        loaders: loaders
+    },
+    output: {
+        path: path.join(__dirname, '../build'),
+        filename: '[name].js'
+    },
+    plugins: plugins,
+    postcss: () => {
+       return [ Autoprefixer({browsers: ['last 20 versions']}) ];
+    }
+};
